4963eab3da8d62d6c7ad2dbfc00172cd4df75e44
Markus Gasser <markus.gasser@mailbox.org>
2023-06-26 20:13:56 +0000

    Prepare reading jwt from request

--- src/scheme/jwt/scheme.rs
+++ src/scheme/jwt/scheme.rs
@@ -1,3 +1,9 @@
+use std::collections::HashMap;
+
+use base64::{engine::general_purpose::STANDARD as BASE64, Engine};
+use jsonwebtoken::{decode, Validation, Algorithm};
+use rocket::{http::Status, serde::json};
+
 use crate::scheme::{AuthenticationScheme, Outcome};
 
 use super::JwtConfig;
@@ -26,8 +32,34 @@ impl AuthenticationScheme for JwtBearer {
         )
     }
 
-    async fn autenticate(&self, _req: &rocket::Request) -> Outcome {
-        todo!()
+    async fn autenticate(&self, req: &rocket::Request) -> Outcome {
+        for header in req.headers().get("Authorization") {
+            // We expect a Basic scheme
+            let Some(token) = header.strip_prefix("Bearer ") else {
+                continue;
+            };
+
+            let token = token.trim();
+
+            let key = &req
+                .rocket()
+                .state::<JwtConfig>()
+                .expect("Missing JwtConfig")
+                .deconding_key;
+            let validation = Validation::new(Algorithm::HS256);
+            
+            let token = match decode::<HashMap<String, json::Value>>(token, key, &validation) {
+                Ok(token) => token,
+                Err(err) => return Outcome::Failure((Status::BadRequest, Box::new(err))),
+            };
+
+            let user = todo!("create user from jwt");
+
+            return Outcome::Success(user);
+        }
+
+        // No Authorization headers, we cannot handle the request
+        Outcome::Forward(())
     }
 
     fn challenge(&self) -> String {
